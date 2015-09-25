b6c6b9fde156992f343b69b7991bf5213df880db
Tomas Slusny <slusnucky@gmail.com>
2015-09-25 22:33:15 +0200

    Nice CLI visuals

--- raxe/cli/Cli.hx
+++ raxe/cli/Cli.hx
@@ -3,8 +3,14 @@ package raxe.cli;
 import mcli.CommandLine;
 
 /**
-    raxe-lang command line tool
-    Transpile your raxe sources into haxe
+    -
+    8b,dPPYba,  ,adPPYYba,  8b,     ,d8  ,adPPYba,  
+    88P'   "Y8  ""     `Y8   `Y8, ,8P'  a8P_____88  
+    88          ,adPPPPP88     )888(    8PP"""""""  
+    88          88,    ,88   ,d8" "8b,  "8b,   ,aa  
+    88          `"8bbdP"Y8  8P'     `Y8  `"Ybbd8"'  
+
+    Raxe 0.0.1 - https://raxe-lang.org
  **/
 class Cli extends CommandLine {
     /**

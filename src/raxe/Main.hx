4250f3978298ff93a6c076367e5d3b1c552d1b7a
Tomas Slusny <slusnucky@gmail.com>
2015-09-30 13:39:27 +0200

    Finally working

--- src/raxe/Main.hx
+++ src/raxe/Main.hx
@@ -6,9 +6,7 @@ import raxe.cli.Cli;
 class Main {
   static function main() {
   	var args = Sys.args();
-  	trace(Sys.getCwd());
   	Sys.setCwd(args.pop());
-  	trace(Sys.getCwd());
     new Dispatch(args).dispatch(new Cli());
   }
 }

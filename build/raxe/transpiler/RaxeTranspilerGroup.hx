f3b7d6a0306ff135110bb79e7811dc07aa4b0488
Tomas Slusny <slusnucky@gmail.com>
2015-10-01 23:47:21 +0200

    Rewrote RaxeTranspilerGroup to Raxe

--- build/raxe/transpiler/RaxeTranspilerGroup.hx
+++ build/raxe/transpiler/RaxeTranspilerGroup.hx
@@ -1,32 +1,33 @@
-package raxe.transpiler;
-
-import raxe.tools.StringHandle;
+package raxe.transpiler;using Lambda;using StringTools;import raxe.tools.StringHandle;
 import sys.io.File;
 
-class RaxeTranspilerGroup extends TranspilerGroup {
-  public function new() {
-    super();
+@:tink class RaxeTranspilerGroup extends TranspilerGroup{
+
+public function new(){
+  super();
+};
+
+dynamic public function transpile(directory : String, file : String) : String{
+  var currentPackage = file.replace(directory, "");
+  currentPackage = currentPackage.replace("\\", "/");
+  var currentModule = currentPackage.substr(currentPackage.lastIndexOf("/") + 1).replace(".rx", "");
+  currentPackage = currentPackage.replace(currentPackage.substr(currentPackage.lastIndexOf("/")), "");
+  currentPackage = currentPackage.replace("/", ".");
+
+  if(currentPackage.charAt(0) == "."){
+    currentPackage = currentPackage.substr(1);
   }
 
-  public function transpile(directory : String, file : String) : String {
-    var currentPackage = StringTools.replace(file, directory, "");
-    currentPackage = StringTools.replace(currentPackage, "\\", "/");
-    var currentModule = StringTools.replace(currentPackage.substr(currentPackage.lastIndexOf("/") + 1), ".rx", "");
-    currentPackage = StringTools.replace(currentPackage, currentPackage.substr(currentPackage.lastIndexOf("/")), "");
-    currentPackage = StringTools.replace(currentPackage, "/", ".");
+  var content = File.getContent(file);
 
-    if (currentPackage.charAt(0) == ".") currentPackage = currentPackage.substr(1);
+  push(new CoreTranspiler().setName(currentModule).setPath(currentPackage));
+  push(new AccessTranspiler());
+  push(new SemicolonTranspiler());
 
-    var content = File.getContent(file);
-
-    push(new CoreTranspiler().setName(currentModule).setPath(currentPackage));
-    push(new AccessTranspiler());
-    push(new SemicolonTranspiler());
-
-    for (transpiler in transpilers) {
-      content = transpiler.transpile(new StringHandle(content, transpiler.tokens()));
-    }
-
-    return content;
+  for(transpiler in transpilers){
+    content = transpiler.transpile(new StringHandle(content, transpiler.tokens()));
   }
+
+  return content;
+};
 }

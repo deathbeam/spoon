c06af604f72d7915abca1d4b5e2571c9a72f8e99
Tomas Slusny <slusnucky@gmail.com>
2015-10-02 21:01:27 +0200

    Ooops, accidentally pushed .raxecompletion

--- .raxecompletion/examples/HelloWorld.hx
+++
@@ -1,7 +0,0 @@
-package examples;using Lambda;using StringTools;class HelloWorld{
-
-static dynamic public function main(){
-  trace("Hello World");
-};
-
-}
--- .raxecompletion/src/raxe/transpiler/Transpiler.hx
+++
@@ -1,7 +0,0 @@
-package src.raxe.transpiler;using Lambda;using StringTools;import raxe.tools.StringHandle;
-
-interface Transpiler{
-
-dynamic public function tokens() : Array<String>;
-dynamic public function transpile(handle : StringHandle) : String;
-}

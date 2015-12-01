29a2bc4c52658fe3c743841f9f1ffd0040bb3a1d
Tomas Slusny <slusnucky@gmail.com>
2015-12-01 20:50:26 +0100

    Lexing for function, class and extends Also, refactored Lexer a bit, so now it is a lot more readable.

--- spoon/lexer/Lexer.hx
+++ spoon/lexer/Lexer.hx
@@ -1,91 +1,129 @@
 package spoon.lexer;
 
 import hxparse.Position;
+import hxparse.RuleBuilder;
 import spoon.log.Message;
 import spoon.log.Logger;
 import spoon.log.LogParser;
 import spoon.lexer.Token;
 
-class Lexer extends hxparse.Lexer implements hxparse.RuleBuilder {
+class Lexer extends hxparse.Lexer implements RuleBuilder {
   public static var tok = @:rule [
-    "\\("   => TPOpen(lexer.curPos()),
-    "\\)"   => TPClose(lexer.curPos()),
-    ","     => TComma(lexer.curPos()),
-    "\\."   => TDot(lexer.curPos()),
-    "if"    => TIf(lexer.curPos()),
-    "else"  => TElse(lexer.curPos()),
-    "for"   => TFor(lexer.curPos()),
-    "while" => TWhile(lexer.curPos()),
-    ""      => TEof(lexer.curPos()),
-    // Constants
-    "true"  => TTrue(lexer.curPos()),
-    "false" => TFalse(lexer.curPos()),
-    "null"  => TNull(lexer.curPos()),
-    // Identifiers and types
-    "_?[A-Z][a-zA-Z\\-]+" => TType(lexer.curPos(), lexer.current),
-    "_?[a-z][a-zA-Z\\-]+" => TVar(lexer.curPos(), lexer.current),
-    // Numbers
-    "0x[0-9a-fA-F]+"                     => TInt(lexer.curPos(), lexer.current),
-    "[0-9]+"                             => TInt(lexer.curPos(), lexer.current),
-    "[0-9]+\\.[0-9]+"                    => TFloat(lexer.curPos(), lexer.current),
-    "\\.[0-9]+"                          => TFloat(lexer.curPos(), lexer.current),
-    "[0-9]+[eE][\\+\\-]?[0-9]+"          => TFloat(lexer.curPos(), lexer.current),
-    "[0-9]+\\.[0-9]*[eE][\\+\\-]?[0-9]+" => TFloat(lexer.curPos(), lexer.current),
-    // Strings
-    '"' => {
-      buf = new StringBuf();
-      var pmin = lexer.curPos();
-      var pmax = pmin;
+    /***************************************************************************
+    * Keywords
+    ***************************************************************************/
+    "if"
+      => TIf(lexer.curPos()),
+    "else"
+      => TElse(lexer.curPos()),
+    "for"
+      => TFor(lexer.curPos()),
+    "while"
+      => TWhile(lexer.curPos()),
+    "function"
+      => TFunction(lexer.curPos()),
+    "class"
+      => TClass(lexer.curPos()),
+    "extends"
+      => TExtends(lexer.curPos()),
+    /***************************************************************************
+    * Constants
+    ***************************************************************************/
+    "true"
+      => TTrue(lexer.curPos()),
+    "false"
+      => TFalse(lexer.curPos()),
+    "null"
+      => TNull(lexer.curPos()),
+    /***************************************************************************
+    * Identifiers and types
+    ***************************************************************************/
+    "_?[A-Z][a-zA-Z\\-]+"
+      => TType(lexer.curPos(), lexer.current),
+    "_?[a-z][a-zA-Z\\-]+"
+      => TVar(lexer.curPos(), lexer.current),
+    /***************************************************************************
+    * Operators and symbols
+    ***************************************************************************/
+    "\\("
+      => TPOpen(lexer.curPos()),
+    "\\)"
+      => TPClose(lexer.curPos()),
+    ","
+      => TComma(lexer.curPos()),
+    "\\."
+      => TDot(lexer.curPos()),
+    ""
+      => TEof(lexer.curPos()),
+    /***************************************************************************
+    * Numbers
+    ***************************************************************************/
+    "0x[0-9a-fA-F]+"
+      => TInt(lexer.curPos(), lexer.current),
+    "[0-9]+"
+      => TInt(lexer.curPos(), lexer.current),
+    "[0-9]+\\.[0-9]+"
+      => TFloat(lexer.curPos(), lexer.current),
+    "\\.[0-9]+"
+      => TFloat(lexer.curPos(), lexer.current),
+    "[0-9]+[eE][\\+\\-]?[0-9]+"
+      => TFloat(lexer.curPos(), lexer.current),
+    "[0-9]+\\.[0-9]*[eE][\\+\\-]?[0-9]+"
+      => TFloat(lexer.curPos(), lexer.current),
+    /***************************************************************************
+    * Strings
+    ***************************************************************************/
+    '"'
+      => TString(process(lexer, string, UnterminatedString), buf.toString()),
+    /***************************************************************************
+    * Comments
+    ***************************************************************************/
+    "#[^#][^\n\r]*"
+      => lexer.token(tok),
+    "###"
+      => TComment(process(lexer, comment, UnclosedComment), buf.toString()),
+    /***************************************************************************
+    * Whitespace skip
+    ***************************************************************************/
+    "[\r\n\t ]"
+      => lexer.token(tok),
+    /***************************************************************************
+    * Blocks (indent/dedent)
+    ***************************************************************************/
+    "\n[ \t]*"
+      => {
+        var last = stack[stack.length - 1];
+        var dent = lexer.current.length - 1;
 
-      try {
-        pmax = lexer.token(string);
-      } catch (e : haxe.io.Eof) {
-        Logger.self.log({
-          type: UnterminatedString,
-          position: pmin,
-          severity: Error
-        });
+        if (dent > last) {
+          stack.push(dent);
+          TIndent(lexer.curPos());
+        } else if (dent < last) {
+          stack.pop();
+          TDedent(lexer.curPos());
+        } else {
+          lexer.token(tok);
+        }
       }
-
-      TString(Position.union(pmin, pmax), buf.toString());
-    },
-    // Comments
-    "#[^#][^\n\r]*" => lexer.token(tok),
-    "###" => {
-      buf = new StringBuf();
-      var pmin = lexer.curPos();
-      var pmax = pmin;
-
-      try {
-        pmax = lexer.token(comment);
-      } catch (e : haxe.io.Eof) {
-        Logger.self.log({
-          type: UnclosedComment,
-          position: pmin,
-          severity: Error
-        });
-      }
-
-      TComment(Position.union(pmin, pmax), buf.toString());
-    },
-    // Indent/dedent
-    "\n[ \t]*" => {
-      var last = stack[stack.length - 1];
-      var dent = lexer.current.length - 1;
-
-      if (dent > last) {
-        stack.push(dent);
-        TIndent(lexer.curPos());
-      } else if (dent < last) {
-        stack.pop();
-        TDedent(lexer.curPos());
-      } else {
-        lexer.token(tok);
-      }
-    },
-    // Skip whitespace
-    "[\r\n\t ]" => lexer.token(tok)
   ];
+
+  static function process(lexer, rule, error) {
+    buf = new StringBuf();
+    var pmin = lexer.curPos();
+    var pmax = pmin;
+
+    try {
+      pmax = lexer.token(rule);
+    } catch (e : haxe.io.Eof) {
+      Logger.self.log({
+        type: error,
+        position: pmin,
+        severity: Error
+      });
+    }
+
+    return Position.union(pmin, pmax);
+  }
 
   static var comment = @:rule [
     "###" => lexer.curPos(),

b37927420a8e290dbcdff674b85fa52e58b6e7f3
Tomas Slusny <slusnucky@gmail.com>
2015-12-02 22:37:39 +0100

    Added hovers, better corner button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
-((window.gitter = {}).chat = {}).options = {
-  room: 'nondev/spoon',
-  activationElement: false
-};
-
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-68102301-1', 'auto');
-ga('send', 'pageview');
 
 var xmlhttp = new XMLHttpRequest();
 var url = "https://api.github.com/repos/nondev/spoon/commits";
@@ -26,6 +18,14 @@ xmlhttp.onreadystatechange = function() {
 }
 
 $(function() {
+  $('[data-toggle="tooltip"]').tooltip();
   xmlhttp.open("GET", url, true);
   xmlhttp.send();
+
+  ((window.gitter = {}).chat = {}).options = {
+    room: 'nondev/spoon',
+    activationElement: false
+  };
+  ga('create', 'UA-68102301-1', 'auto');
+  ga('send', 'pageview');
 });

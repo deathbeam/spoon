5d92ddf806119d2e1c366eb47e58001bc3c811df
Tomas Slusny <slusnucky@gmail.com>
2016-05-26 10:11:59 +0200

    Removed CodeClimate test reported, Coveralls are better

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'coveralls'
 Coveralls.wear!
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
 require 'parslet/rig/rspec'
 require 'spoon'
